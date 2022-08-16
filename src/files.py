9a62ae5231ddb2a81003a208abe0c4764b17b8ea
Widmark-Kaue <widmarkkaue.s.c@gmail.com>
2022-08-16 19:05:52 -0300

    import new function sub

--- src/files.py
+++ src/files.py
@@ -1,6 +1,5 @@
 import os
-from ssl import ALERT_DESCRIPTION_UNSUPPORTED_CERTIFICATE
-
+from re import sub 
 
 def open_estability(file_name: str) -> dict:
     aircraft = {}
@@ -24,16 +23,27 @@ def open_massProp(file_name: str) -> dict:
         for l in file:
             aux = l.replace('  ', '')
             aux = aux.replace('\n', '')
-            auxl = aux.split('\t')
-            if auxl[0] == 'Totals':
-                totals = auxl.copy()
-            if auxl[0] == 'Name':
-                name = auxl.copy()
+            aux = aux.split('\t')
+            if aux[0] == 'Totals':
+                totals = aux.copy()
+            if aux[0] == 'Name':
+                name = aux.copy()
     for i in range(1, len(name)):
         aircraft[name[i]] = float(totals[i])
     return aircraft
 
 def open_waveDrag(file_name:str) -> dict:
+    aircraft = {}
+    with open(file_name) as file:
+        for l in file:
+            aux = l.replace('  ', '')
+            aux = aux.replace('\n', '')
+            aux = aux.split(':')
+            if aux[0] == 'CdWave':
+                aircraft[aux[0]] = float(aux[1])
+    return aircraft
+
+def open_compGeom(file_name: str) -> dict:
     aircraft = {}
     name = []
     totals = []
@@ -41,7 +51,11 @@ def open_waveDrag(file_name:str) -> dict:
         for l in file:
             aux = l.replace('  ', '')
             aux = aux.replace('\n', '')
-            auxl = aux.split(':')
-            if auxl[0] == 'CdWave':
-                aircraft[auxl[0]] = float(auxl[1])
+            aux = aux.split('\t')
+            if aux[-1] == 'Totals':
+                totals = aux.copy()
+            if aux[-1] == 'Name':
+                name = aux.copy()
+    for i in range(1, len(name)):
+        aircraft[name[i]] = float(totals[i])
     return aircraft
