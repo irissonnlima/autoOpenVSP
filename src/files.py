0bf76319eeb5420c8bdd0b01d8e7df4e9334493b
Widmark-Kaue <widmarkkaue.s.c@gmail.com>
2022-08-16 18:39:26 -0300

    waveDrag func add

--- src/files.py
+++ src/files.py
@@ -1,4 +1,5 @@
 import os
+from ssl import ALERT_DESCRIPTION_UNSUPPORTED_CERTIFICATE
 
 
 def open_estability(file_name: str) -> dict:
@@ -30,4 +31,17 @@ def open_massProp(file_name: str) -> dict:
                 name = auxl.copy()
     for i in range(1, len(name)):
         aircraft[name[i]] = float(totals[i])
+    return aircraft
+
+def open_waveDrag(file_name:str) -> dict:
+    aircraft = {}
+    name = []
+    totals = []
+    with open(file_name) as file:
+        for l in file:
+            aux = l.replace('  ', '')
+            aux = aux.replace('\n', '')
+            auxl = aux.split(':')
+            if auxl[0] == 'CdWave':
+                aircraft[auxl[0]] = float(auxl[1])
     return aircraft
